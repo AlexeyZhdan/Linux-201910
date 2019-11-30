dc923f69161ad0455227fc0c022461fe3b931c67
AlexeyZhdan <57165540+AlexeyZhdan@users.noreply.github.com>
2019-11-30 23:36:16 +0300

    Correction

--- HW-7/repo/mkrepo.sh
+++ HW-7/repo/mkrepo.sh
@@ -4,7 +4,8 @@ mkdir /usr/share/nginx/html/repo
 cp rpmbuild/RPMS/x86_64/nginx-1.16.1-1.el7.ngx.x86_64.rpm /usr/share/nginx/html/repo
 wget http://www.percona.com/downloads/percona-release/redhat/0.1-6/percona-release-0.1-6.noarch.rpm -O /usr/share/nginx/html/repo/percona-release-0.1-6.noarch.rpm
 createrepo /usr/share/nginx/html/repo/
-cp -f /files/default.conf /etc/nginx/conf.d/nginx -s reload
+cp -f /files/default.conf /etc/nginx/conf.d/
+nginx -s reload
 cat >> /etc/yum.repos.d/myrepo.repo <<EOF
  [myrepo]
  name=myrepo-linux
@@ -12,4 +13,3 @@ cat >> /etc/yum.repos.d/myrepo.repo <<EOF
  gpgcheck=0
  enabled=1
  EOF
- 
